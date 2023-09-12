7f47619c8c94326c0e69ffc91b9231ae270c0efb
jamesrmathis <jrfmathis06@outlook.com>
2023-09-12 10:55:32 -0500

    refactored code

--- main.py
+++ main.py
@@ -1,89 +1,124 @@
-file = open(r'C:\Users\James\Documents\Code\School\ISEF Project\functions.txt', 'r').read()
+def parseData():
+    file = open(r'C:\Users\James\Documents\Code\School\ISEF Project\functions.txt', 'r').read()
 
-functions = file.split('\n<sep>\n')
-results = []
-for i in range(len(functions)):
-    for line in functions[i].split('\n'):
-        if line.startswith('#'):
-            results.append(int(line[1:].strip()))
-            functions[i] = functions[i].replace(line, '')
+    functions = file.split('\n<sep>\n')
+    results = []
+    for i in range(len(functions)):
+        for line in functions[i].split('\n'):
+            if line.startswith('#'):
+                results.append(int(line[1:].strip()))
+                functions[i] = functions[i].replace(line, '')
+    
+    return functions, results
 
-# Shuffle the data
-import random
+def shuffleData(functions, results):
+    # Shuffle the data
+    import random
 
-data = list(zip(functions, results))
-random.shuffle(data)
-functions, results = zip(*data)
+    data = list(zip(functions, results))
+    random.shuffle(data)
+    funcs, res = zip(*data)
+    return funcs, res
 
-# Tokenize the data
-from keras.preprocessing.text import Tokenizer
+def tokenizeData(functions):
+    
+    # Tokenize the data
+    from keras.preprocessing.text import Tokenizer
 
-tokenizer = Tokenizer(num_words=1000)
-tokenizer.fit_on_texts(functions)
-sequences = tokenizer.texts_to_sequences(functions)
+    tokenizer = Tokenizer(num_words=1000)
+    tokenizer.fit_on_texts(functions)
+    sequences = tokenizer.texts_to_sequences(functions)
 
-# Pad the sequences to a fixed length
-from keras.preprocessing.sequence import pad_sequences
+    # Pad the sequences to a fixed length
+    from keras.preprocessing.sequence import pad_sequences
 
-max_length = 100
-padded_sequences = pad_sequences(sequences, maxlen=max_length, padding='post')
+    max_length = 100
+    padded_sequences = pad_sequences(sequences, maxlen=max_length, padding='post')
 
-# Split the data into training and testing sets
-import numpy as np
+    return sequences, padded_sequences, max_length, tokenizer
 
-split_index = int(len(padded_sequences) * 2 / 3)
-X_train = padded_sequences[:split_index]
-y_train = np.array(results[:split_index])
-X_test = padded_sequences[split_index:]
-y_test = np.array(results[split_index:])
+def splitData(padded_sequences, results):
+    # Split the data into training and testing sets
+    import numpy as np
 
-# Define the model architecture
-from keras.models import Sequential
-from keras.layers import Embedding, Flatten, Dense
-from keras.metrics import Recall, Precision
+    split_index = int(len(padded_sequences) * 2 / 3)
+    X_train = padded_sequences[:split_index]
+    y_train = np.array(results[:split_index])
+    X_test = padded_sequences[split_index:]
+    y_test = np.array(results[split_index:])
 
-model = Sequential()
-model.add(Embedding(input_dim=1000, output_dim=10, input_length=max_length))
-model.add(Flatten())
-model.add(Dense(1, activation='sigmoid'))
+    return X_train, y_train, X_test, y_test
 
-# Compile and train the model
-import pickle
-training = 1
-if training:
-    # recall = Recall()
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', 'Recall', 'Precision'])
-    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=1000, batch_size=10, verbose=0)
-    model.save('model.keras')
+def createModel(max_length):
 
-    pickle.dump(X_test, open('X_test.pkl', 'wb'))
-    pickle.dump(y_test, open('y_test.pkl', 'wb'))
-else:
-    from keras.models import load_model
 
-    model = load_model('model.keras')
-    X_test = pickle.load(open('X_test.pkl', 'rb'))
-    y_test = pickle.load(open('y_test.pkl', 'rb'))
+    # Define the model architecture
+    from keras.models import Sequential
+    from keras.layers import Embedding, Flatten, Dense
+    from keras.metrics import Recall, Precision
 
-# Evaluate the model
-loss, accuracy, recall, precision = model.evaluate(X_test, y_test)
-# print('Test accuracy:', accuracy)
+    model = Sequential()
+    model.add(Embedding(input_dim=1000, output_dim=10, input_length=max_length))
+    model.add(Flatten())
+    model.add(Dense(1, activation='sigmoid'))
 
-y_pred = model.predict(X_test)
-y_pred = (y_pred > 0.5)
-# print(p)
+    return model
 
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from matplotlib import pyplot as plt
-cm = confusion_matrix(y_test, y_pred, labels=[0, 1])
-disp = ConfusionMatrixDisplay(cm, display_labels=['doesnt halt', 'halts'])
-disp.plot(cmap=plt.cm.Reds)
-plt.show()
+def trainModel(model, X_train, y_train, X_test, y_test):
+    
+    # Compile and train the model
+    import pickle
+    training = 1
+    if training:
+        # recall = Recall()
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', 'Recall', 'Precision'])
+        model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=1000, batch_size=10, verbose=0)
+        model.save('model.keras')
 
-# # Create a reverse word index
-# reverse_word_index = dict([(value, key) for (key, value) in tokenizer.word_index.items()])
+        pickle.dump(X_test, open('X_test.pkl', 'wb'))
+        pickle.dump(y_test, open('y_test.pkl', 'wb'))
+    else:
+        from keras.models import load_model
 
-# # Convert a sequence of integers back into text
-# for ind, sequence in enumerate(X_test):
-#     text = ' '.join([reverse_word_index.get(i, '\0') for i in sequence])
-#     print(f'real: {y_test[ind]}, predicted: {p[ind]}, text: {text}')
+        model = load_model('model.keras')
+        X_test = pickle.load(open('X_test.pkl', 'rb'))
+        y_test = pickle.load(open('y_test.pkl', 'rb'))
+
+    # Evaluate the model
+    loss, accuracy, recall, precision = model.evaluate(X_test, y_test)
+
+    y_pred = model.predict(X_test)
+    y_pred = (y_pred > 0.5)
+
+def createConfusionMatrix(y_test, y_pred):
+    from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+    from matplotlib import pyplot as plt
+    cm = confusion_matrix(y_test, y_pred, labels=[0, 1])
+    disp = ConfusionMatrixDisplay(cm, display_labels=['doesnt halt', 'halts'])
+    disp.plot(cmap=plt.cm.Reds)
+    plt.show()
+
+def seePredictions(X_test, y_test, y_pred,  tokenizer):
+
+    # Create a reverse word index
+    reverse_word_index = dict([(value, key) for (key, value) in tokenizer.word_index.items()])
+
+    # Convert a sequence of integers back into text
+    for ind, sequence in enumerate(X_test):
+        text = ' '.join([reverse_word_index.get(i, '\0') for i in sequence])
+        print(f'real: {y_test[ind]}, predicted: {y_pred[ind]}, text: {text}')
+
+def main():
+    functions, results = parseData()
+    functions, results = shuffleData(functions, results)
+    sequences, padded_sequences, max_length, tokenizer = tokenizeData(functions)
+    X_train, y_train, X_test, y_test = splitData(padded_sequences, results)
+    model = createModel(max_length)
+    trainModel(model, X_train, y_train, X_test, y_test)
+    y_pred = model.predict(X_test)
+    y_pred = (y_pred > 0.5)
+    createConfusionMatrix(y_test, y_pred)
+    seePredictions(X_test, y_test, y_pred, tokenizer)
+
+if __name__ == '__main__':
+    main()
