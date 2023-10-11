ace82c1792d8c57bb48f2b4fe3def7b3123e4ccf
jamesrmathis <jrfmathis06@outlook.com>
2023-10-11 09:14:06 -0500

    added break statements

--- funcGenerator.py
+++ funcGenerator.py
@@ -23,7 +23,7 @@ def generate_random_function():
 
 def generate_random_statement(indentation_level=1):
     # Choose a random statement type
-    statement_type = random.choice(["assignment", "conditional", "loop", "return", "break"])
+    statement_type = random.choice(["assignment", "conditional", "loop", "return"])
 
     if statement_type == "assignment":
         return generate_assignment_statement(indentation_level)
@@ -33,8 +33,8 @@ def generate_random_statement(indentation_level=1):
         return generate_loop_statement(indentation_level)
     elif statement_type == "return":
         return generate_return_statement(indentation_level)
-    elif statement_type == "break":
-        return generate_break_statement(indentation_level)
+    # elif statement_type == "break":
+    #     return generate_break_statement(indentation_level)
 
 def generate_assignment_statement(indentation_level):
     variable_name = f"var_{random.randint(1, 100)}"
@@ -54,18 +54,24 @@ def generate_conditional_statement(indentation_level):
 def generate_loop_statement(indentation_level):
     indentation = "    " * indentation_level
     loop_type = random.choice(["for", "while"])
+    should_break = random.randint(0, 1)
     
     if loop_type == "for":
         loop_variable = f"i_{random.randint(1, 10)}"
         loop_range = random.randint(1, 10)
-        return f"{indentation}for {loop_variable} in range({loop_range}):\n{generate_random_statement(indentation_level + 1)}"
+        statement = f"{indentation}for {loop_variable} in range({loop_range}):\n{generate_random_statement(indentation_level + 1)}"
+        statement += f"\n{generate_break_statement(statement)}" if should_break else ""
+        return statement
     else:
         boolean = random.randint(0, 1)
         if boolean:
             condition = random.choice(["True", "False"])
         else:
             condition = generate_complex_condition()
-        return f"{indentation}while {condition}:\n{generate_random_statement(indentation_level + 1)}"
+
+        statement = f"{indentation}while {condition}:\n{generate_random_statement(indentation_level + 1)}"
+        statement += f"\n{generate_break_statement(statement)}" if should_break else ""
+        return statement
     
 def generate_complex_condition():
     # Generate a random mathematical expression as a condition
@@ -79,26 +85,26 @@ def generate_return_statement(indentation_level):
     indentation = "    " * indentation_level
     return f"{indentation}return {return_value}"
 
-def generate_break_statement(indentation_level):
-    indentation = "    " * (indentation_level + 1)
-    return f"{indentation}break"
-    # Split the function code into lines
-    # lines = function_code.splitlines()
-
-    # last_loop_indentation = 0
-    # # Iterate over the lines in reverse order to find the last for or while loop
-    # for i in range(len(lines) - 1, -1, -1):
-    #     line = lines[i].strip()
-    #     if line.startswith("for") or line.startswith("while"):
-    #         # Return the indentation level of the last loop
-    #         last_loop_indentation = (len(line) - len(line.lstrip())) // 4
-
-    # # Return None if no loops were found
-    # if last_loop_indentation == 0:
-    #     return None
-
-    # indentation = "    " * (last_loop_indentation - 1)
+def generate_break_statement(function_code):
+    # indentation = "    " * (indentation_level + 1)
     # return f"{indentation}break"
+    # Split the function code into lines
+    lines = function_code.splitlines()
+
+    last_loop_indentation = 0
+    # Iterate over the lines in reverse order to find the last for or while loop
+    for i in range(len(lines) - 1, -1, -1):
+        line = lines[i]
+        if 'for' in line or 'while' in line:
+            # Return the indentation level of the last loop
+            last_loop_indentation = (len(line) - len(line.lstrip())) // 4
+
+    # Return None if no loops were found
+    if last_loop_indentation == 0:
+        return None
+
+    indentation = "    " * (last_loop_indentation + 1)
+    return f"{indentation}break"
 
 
 def run_with_timeout(func, args=(), timeout=10):
@@ -125,12 +131,12 @@ def run_with_timeout(func, args=(), timeout=10):
     # If the thread is not alive, return the result
     return True
 
-for _ in range(100000):
+for _ in range(1000):
     import re
     import time
 
     random_function = generate_random_function()
-    print(random_function)
+    # print(random_function)
     fn_name = re.findall(r'func_\w*', random_function)[0]
     start = time.perf_counter() * 1e9
     exec(random_function)
